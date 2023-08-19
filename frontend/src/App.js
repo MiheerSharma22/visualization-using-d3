069cf9cc1a4e28d8755675ca5bf0ef8840ec2d95
MiheerSharma22 <miheer.sharma1@gmail.com>
2023-08-19 19:20:30 +0530

    installed tailwind in frontend and added concurrrently script

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,11 @@
 import "./App.css";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="text-4xl font-bol text-blue-600 text-center">
+      Hi this is assignment of blackcoffer
+    </div>
+  );
 }
 
 export default App;
