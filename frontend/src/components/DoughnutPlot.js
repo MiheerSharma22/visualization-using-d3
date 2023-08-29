a9c2f6bea229e5bae28ad8feb85bee683086bb25
MiheerSharma22 <miheer.sharma1@gmail.com>
2023-08-29 14:24:11 +0530

    added label and tooltip on interaction with doughnut chart

--- frontend/src/components/DoughnutPlot.js
+++ frontend/src/components/DoughnutPlot.js
@@ -29,7 +29,6 @@ const DoughnutPlot = ({ data, width, height }) => {
     if (d.sector === "") sectorMap.others++;
     else sectorMap[d.sector]++;
   }
-  console.log("sectors: ", sectorMap);
 
   useEffect(() => {
     drawChart();
@@ -55,31 +54,41 @@ const DoughnutPlot = ({ data, width, height }) => {
     const color = d3
       .scaleOrdinal()
       .range([
-        "#00A896",
-        "#02C39A",
-        "#00B4A0",
-        "#3DCCA6",
-        "#4CAF50",
-        "#43AA8B",
-        "#61C0BF",
-        "#48BFE3",
-        "#6A4C93",
-        "#6B4226",
-        "#70C1B3",
-        "#34A853",
-        "#227D51",
-        "#1B998B",
-        "#83C5BE",
-        "#3C493F",
-        "#61A6AB",
-        "#9A8C98",
-        "#5C755E",
+        "#28c76f",
+        "#36cd77",
+        "#43d28066",
+        "#51d888",
+        "#5fde90",
+        "#6ce497",
+        "#7aeb9f",
+        "#88f2a7",
+        "#95f9af",
+        "#a3ffb7",
+        "#b0ffc0",
+        "#beffc8",
+        "#ccffd0",
+        "#d9ffd8",
+        "#e7ffdf",
+        "#f5ffe7",
+        "#ffffff",
       ]);
 
     // Compute the position of each group on the pie:
     const pie = d3.pie().value((d) => d[1]);
 
     const data_ready = pie(Object.entries(sectorMap));
+
+    // adding label
+    svg
+      .append("text")
+      .attr("class", "label")
+      .attr("x", -150)
+      .attr("y", 0.55 * height)
+      .attr("fill", "#7367f0")
+      .text("Number of records under each Sector");
+
+    // adding tooltip
+    var tooltip = d3.select("#container2").append("div").style("opacity", 0);
 
     svg
       .selectAll("whatever")
@@ -92,16 +101,32 @@ const DoughnutPlot = ({ data, width, height }) => {
           .innerRadius(120) // This is the size of the donut hole
           .outerRadius(radius)
       )
+      .on("mouseover", (e, d) => {
+        tooltip.transition().duration(200).style("opacity", 0.9);
+        tooltip.html(
+          ` <div class="doughnut-tooltip-container">
+              <p><span>Sector:</span> ${d.data[0]}</p> 
+              <p><span>No. of Records:</span> ${d.data[1]}</p>
+            </div>
+             `
+        );
+      })
+      .on("mouseout", (d) => {
+        tooltip.transition().duration(500).style("opacity", 0);
+      })
       .attr("fill", (d) => color(d.data[0]))
-      .attr("stroke", "black")
-      .style("stroke-width", "2px")
+      // .attr("stroke", "#d9ffd8")
+      // .style("stroke-width", "1px")
       .style("opacity", 0.7);
   }
 
   return (
     <div className="w-9/12 mx-auto flex flex-col border border-borderColor px-[1.3rem] py-[0.75rem] rounded-lg">
       <p className="text-textColor text-xl font-medium">Doughnut Chart</p>
-      <div id="container2" className="place-self-center "></div>
+      <div
+        id="container2"
+        className="place-self-center flex flex-col-reverse mb-10"
+      ></div>
     </div>
   );
 };
