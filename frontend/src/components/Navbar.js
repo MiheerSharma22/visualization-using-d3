54ceda2e533ba324dcc706870722ad13fc9dfa7c
MiheerSharma22 <miheer.sharma1@gmail.com>
2023-09-01 13:18:01 +0530

    removed dummy pic

--- frontend/src/components/Navbar.js
+++ frontend/src/components/Navbar.js
@@ -1,18 +1,11 @@
 import React from "react";
-import dummyImage from "../assets/dummyUser.png";
 
 const Navbar = () => {
   return (
     <div className="w-9/12 mx-auto border border-borderColor flex items-center justify-between px-[2rem] py-[1rem] rounded-lg">
-      <p className="text-textColor text-2xl font-semibold">
+      <p className="text-textColor text-center text-2xl font-semibold">
         Chart Visualisation
       </p>
-
-      <img
-        src={dummyImage}
-        alt="dummyUser"
-        className="rounded-full w-[45px] aspect-square"
-      />
     </div>
   );
 };
