6f2dd71889653fb0926b4c3b0e10ab1d9acff399
MiheerSharma22 <miheer.sharma1@gmail.com>
2023-09-01 12:46:35 +0530

    removed / from hosted url

--- server/index.js
+++ server/index.js
@@ -15,7 +15,7 @@ const PORT = process.env.PORT || 5000;
 app.use(express.json());
 
 const allowedOrigins = [
-  "https://visualization-using-d3-frontend.vercel.app/",
+  "https://visualization-using-d3-frontend.vercel.app",
   "http://localhost:3000",
 ];
 app.use(
