1e37d9ce97ad31247a86ff04a9da7cce3330f726
MiheerSharma22 <miheer.sharma1@gmail.com>
2023-09-01 13:19:00 +0530

    centered heading in navbar

--- frontend/src/components/Navbar.js
+++ frontend/src/components/Navbar.js
@@ -3,7 +3,7 @@ import React from "react";
 const Navbar = () => {
   return (
     <div className="w-9/12 mx-auto border border-borderColor flex items-center justify-between px-[2rem] py-[1rem] rounded-lg">
-      <p className="text-textColor text-center text-2xl font-semibold">
+      <p className="text-textColor text-center text-2xl font-semibold w-full">
         Chart Visualisation
       </p>
     </div>
